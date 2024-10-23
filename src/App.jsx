4b3341f8e7b6304d0f99e8cb4e47cbe8bdb9b4bb
riad <ahsunriad@gmail.com>
2024-10-23 14:23:45 +0600

    learnt & created component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Login, { Profile } from './UserContent'
+
+function App() {
+  return (
+    <>
+    <h1>Hello World from React JS!</h1>
+    <Login/>
+    <Profile/>
+    </>
+  )
+}
+
+export default App
---
+++ src/UserContent.jsx
@@ -0,0 +1,13 @@
+function Login() {
+    return(
+        <div>
+            <h2>This is from UserContent file</h2>
+        </div>
+    )
+}
+export default Login;
+export function Profile() {
+    return(
+        <h3>This is from another function.</h3>
+    )
+}
